8bef47ce150a0fa8aee813029a025d84ebdae5d8
Dmitry543 <dmitry.b.third@gmail.com>
2024-09-27 12:16:40 +0300

    Added simple test.

--- src/test/java/com/hackernoon/aidemo/AiDemoApplicationTests.java
+++ src/test/java/com/hackernoon/aidemo/AiDemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.hackernoon.aidemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AiDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Test
+    public void shouldReceiveResponseWithOKStatusCode() {
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/ai/generate?message=I%20love%20coffee",String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
